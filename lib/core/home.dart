ccc55c2a26bbaa254f57de006b85203fb9b2f86e
Mahmoud_Madih <moodbeder150@gmail.com>
2024-03-30 02:36:49 +0200

    home Qoyran screen

--- lib/core/home.dart
+++ lib/core/home.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:flutter/rendering.dart';
 
 class Home extends StatefulWidget {
-  static String ROUTE_NAME="home";
+  static String ROUTE_NAME = "home";
 
   @override
   State<Home> createState() => _HomeState();
@@ -13,21 +15,44 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-        image: DecorationImage(image:AssetImage("assets/images/default_bg.png"))
-      ),
+          image: DecorationImage(
+              image: AssetImage("assets/images/default_bg.png"))),
       child: Scaffold(
-        backgroundColor: Colors.transparent,
-        appBar: AppBar(
           backgroundColor: Colors.transparent,
-          elevation: 0,
-          centerTitle: true,
-          title: Text("اسلامي",style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 30,
-          ),),
-        ),
-
-      ),
+          appBar: AppBar(
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            centerTitle: true,
+            title: Text(
+              "اسلامي",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 30,
+              ),
+            ),
+          ),
+          bottomNavigationBar: Theme(
+            data: ThemeData(
+              canvasColor: Color(0xffB7935F)
+            ),
+            child: BottomNavigationBar(
+              backgroundColor: const Color(0xffB7935F),
+              items: [
+                BottomNavigationBarItem(
+                    label: "Qouran",
+                    icon: ImageIcon(AssetImage("assets/images/icon_quran.png"))),
+                BottomNavigationBarItem(
+                    label: "hedeth",
+                    icon: ImageIcon(AssetImage("assets/images/icon_hadeth.png"))),
+                BottomNavigationBarItem(
+                    label: "Qouran",
+                    icon: ImageIcon(AssetImage("assets/images/icon_radio.png"))),
+                BottomNavigationBarItem(
+                    label: "Qouran",
+                    icon: ImageIcon(AssetImage("assets/images/icon_sebha.png"))),
+              ],
+            ),
+          )),
     );
   }
 }
