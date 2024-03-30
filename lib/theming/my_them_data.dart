f9f4ce80768b24c2d735f503b16f8f96c9651a25
Mahmoud_Madih <moodbeder150@gmail.com>
2024-03-30 04:54:15 +0200

    sura Name

---
+++ lib/theming/my_them_data.dart
@@ -0,0 +1,6 @@
+import 'dart:ui';
+
+class ThemData{
+  static Color primryColor = Color(0xffB7935F);
+  static Color scandColor = Color(0xff0F1424);
+}
--- lib/widget_fragment/quran/quran.dart
+++ lib/widget_fragment/quran/quran.dart
@@ -1,12 +1,67 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:islameapp/widget_fragment/quran/soura_name.dart';
+
+import '../../theming/my_them_data.dart';
 
 class quarnFragmant extends StatelessWidget {
-  const quarnFragmant({super.key});
+  List<String> suraNames =["الفاتحه","البقرة","آل عمران","النساء","المائدة","الأنعام","الأعراف","الأنفال","التوبة","يونس","هود"
+    ,"يوسف","الرعد","إبراهيم","الحجر","النحل","الإسراء","الكهف","مريم","طه","الأنبياء","الحج","المؤمنون"
+    ,"النّور","الفرقان","الشعراء","النّمل","القصص","العنكبوت","الرّوم","لقمان","السجدة","الأحزاب","سبأ"
+    ,"فاطر","يس","الصافات","ص","الزمر","غافر","فصّلت","الشورى","الزخرف","الدّخان","الجاثية","الأحقاف"
+    ,"محمد","الفتح","الحجرات","ق","الذاريات","الطور","النجم","القمر","الرحمن","الواقعة","الحديد","المجادلة"
+    ,"الحشر","الممتحنة","الصف","الجمعة","المنافقون","التغابن","الطلاق","التحريم","الملك","القلم","الحاقة","المعارج"
+    ,"نوح","الجن","المزّمّل","المدّثر","القيامة","الإنسان","المرسلات","النبأ","النازعات","عبس","التكوير","الإنفطار"
+    ,"المطفّفين","الإنشقاق","البروج","الطارق","الأعلى","الغاشية","الفجر","البلد","الشمس","الليل","الضحى","الشرح"
+    ,"التين","العلق","القدر","البينة","الزلزلة","العاديات","القارعة","التكاثر","العصر",
+    "الهمزة","الفيل","قريش","الماعون","الكوثر","الكافرون","النصر","المسد","الإخلاص","الفلق","الناس"];
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: BoxDecoration(color: Color(0xff0F1424)),
-    ) ;
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+              flex: 3,
+              child: Image.asset("assets/images/qur2an_screen_logo.png")),
+          Container(
+            width: double.infinity,
+            color: ThemData.primryColor,
+            height: 2,
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(vertical :4),
+            child: Text(
+              "اسم السورة",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+
+                fontWeight: FontWeight.bold,
+                fontSize: 30,
+              ),
+            ),
+          ),
+          Container(
+            width: double.infinity,
+            color: ThemData.primryColor,
+            height: 2,
+          ),
+          Expanded(
+              flex: 7,
+              child: Container(
+                child:ListView.separated(
+                  separatorBuilder:(context,index){
+                    return Divider(color:ThemData.primryColor,thickness: 2,);
+                  },
+                  itemCount: suraNames.length,
+                    itemBuilder: (context ,index){
+                  return suraName(suraNames[index]);
+                }) ,
+
+              )),
+        ],
+      ),
+    );
   }
 }
