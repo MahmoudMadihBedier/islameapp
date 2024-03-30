7c7bab823d3eed3c46d34da742e8376a23be47b9
Mahmoud_Madih <moodbeder150@gmail.com>
2024-03-30 02:01:52 +0200

    no message

---
+++ lib/core/home.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  static String ROUTE_NAME="home";
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(image:AssetImage("assets/images/default_bg.png"))
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text("اسلامي",style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
+          ),),
+        ),
+
+      ),
+    );
+  }
+}
