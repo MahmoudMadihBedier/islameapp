139a8425b94d61eab97559d7c197883ec25fe40b
Mahmoud_Madih <moodbeder150@gmail.com>
2024-03-31 03:17:38 +0200

    sura widget details finsh with bug

--- lib/core/suraWidget/suraWidget.dart
+++ lib/core/suraWidget/suraWidget.dart
@@ -14,62 +14,85 @@ class suraWidget extends StatefulWidget {
 }
 
 class _suraWidgetState extends State<suraWidget> {
-  String suraContant ='';
-  List<String>suraLines=[];
+  String suraContant = '';
+  List<String>suraLines = [];
+
   @override
   Widget build(BuildContext context) {
-    SuraDetailsArgs args =ModalRoute.of(context)!.settings.arguments as SuraDetailsArgs ;
+    SuraDetailsArgs args = ModalRoute
+        .of(context)!
+        .settings
+        .arguments as SuraDetailsArgs;
     readSuraFile(args.fileName);
-    return Container(
-      margin: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
-      padding: EdgeInsets.all(8),
-      color: Colors.black26,
-      child:  Column(
-        mainAxisSize: MainAxisSize.min,
+    return Scaffold(
+      backgroundColor: Colors.white30,
+      endDrawerEnableOpenDragGesture: true,
+
+      // margin: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+      // padding: EdgeInsets.all(8),
+      // color: Colors.black26,
+      body:
+      Container(
+        margin: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+        padding: EdgeInsets.all(8),
+
+        child: Column(
+
+          mainAxisSize: MainAxisSize.min,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
           Row(
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              ElevatedButton(
-                      onPressed: (){},
-                      child: Icon(
-                        Icons.play_arrow
-                      ),
-                    ),
-                    Text(
-                      textAlign: TextAlign.start,
-                      args.suraName,style: TextStyle(
-                        fontSize: 30 ,
-                        fontWeight: FontWeight.bold),),
-
-            ],
-          ),
-            Container(
-              width: double.infinity,
-              color: ThemData.primryColor,
-              height: 2,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ElevatedButton(
+              onPressed: () {},
+              child: Icon(
+                  Icons.play_arrow
+              ),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: ThemData.primryColor,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(60.0),
+                ),
+              ),
             ),
-            // Container(
-            //     child: ListView.separated(
-            //       itemCount: suraLines.length,
-            //       itemBuilder: (context, index) {
-            //         return SuaraView(suraLines[index],index);
-            //       },
-            //       separatorBuilder: (context, index) {
-            //         return Divider(color: ThemData.primryColor,thickness: 2,);
-            //       },
-            //     )
-            // ),
+            Text(
+              textAlign: TextAlign.start,
+              args.suraName, style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold),),
+
+          ],
+        ),
+        Container(
+          width: double.infinity,
+          color: ThemData.primryColor,
+          height: 2,
+        ),
+        //   suraContant.isEmpty?
+        // Center(child:CircularProgressIndicator()) :
+        Expanded(
+            child: Container(
+              child: ListView.builder(
+                itemCount: suraLines.length,
+                itemBuilder: (context, index) {
+                  return SuaraView(suraLines[index], index);
+                },
+              ),
+            ),)
 
 
-    ]),);
+            ]),
+      ),
+    );
   }
-  void readSuraFile(String fileName) async{
-    String fileComtant = await rootBundle.loadString('assets/files/${fileName}');
-    suraContant=fileComtant;
-    suraContant=suraContant.trim();
-    suraLines=suraContant.split('\n');
+
+  void readSuraFile(String fileName) async {
+    String fileComtant = await rootBundle.loadString(
+        'assets/files/${fileName}');
+    suraContant = fileComtant;
+    suraContant = suraContant.trim();
+    suraLines = suraContant.split('\n');
     setState(() {
 
     });
