04320b8f10c82cdfd6d6d93d1588fafd7cba5681
Mahmoud_Madih <moodbeder150@gmail.com>
2024-03-31 03:27:31 +0200

    finishing sura widget witout bugs

--- lib/widget_fragment/quran/suraView.dart
+++ lib/widget_fragment/quran/suraView.dart
@@ -1,25 +1,32 @@
 import 'package:flutter/cupertino.dart';
 
 class SuaraView extends StatelessWidget {
-  int index ;
- String View;
-SuaraView(this.View,this.index);
+  int index;
+
+  String View;
+
+  SuaraView(this.View, this.index);
 
   @override
   Widget build(BuildContext context) {
-
-    View=(index+1).toString()+View;
+    // View=(index+1).toString()+View;
     return Container(
       margin: EdgeInsets.all(8),
       padding: EdgeInsets.all(8),
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text('${View}' ,maxLines: 6,textHeightBehavior: TextHeightBehavior(applyHeightToLastDescent: true),textAlign: TextAlign.end,style: TextStyle(
-            fontWeight: FontWeight.w400,
-            fontSize: 18
-          ),),
-        ],
-      ),
-    );
+                 Expanded(
+                       child:Text('${View}',
+                      softWrap: true,
+                     maxLines: 6,
+                      textHeightBehavior: TextHeightBehavior(applyHeightToLastDescent: true),
+                      textAlign: TextAlign.center,
+                            style: TextStyle(
+                         fontWeight: FontWeight.w400,
+                      fontSize: 18
+        ),)),
+      ],
+    ),);
   }
 }
